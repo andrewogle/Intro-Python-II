277c709117ecc3f96b83998d3cf476c0ec339a5c
Andrew <andrewryanogle@gmail.com>
2019-11-21 19:31:57 -0800

    finished game loop and movement functions

--- src/adv.py
+++ src/adv.py
@@ -12,20 +12,20 @@ from player import Player
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
+    'outside':  Room("outside",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow passage':   Room("narrow passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure chamber': Room("treasure chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -36,11 +36,11 @@ earlier adventurers. The only exit is to the south."""),
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
+room['foyer'].e_to = room['narrow passage']
 room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room['narrow passage'].w_to = room['foyer']
+room['narrow passage'].n_to = room['treasure chamber']
+room['treasure chamber'].s_to = room['narrow passage']
 
 #
 # Main
@@ -83,14 +83,64 @@ def help_menu():
     title_screen_selections()
 
 def print_location():
-    print('\n' + ('#' * (4 + len(player.location))))
-    print('# ' + player.location + ' #')
-    print('# ' + room[player.location].discription + ' #')
-    print('\n' + ('#' * (4 + len(player.location))))
+    print('\n' + ('#' * (4 + len(player.location.name))))
+    print('# ' + player.location.name + ' #')
+    print('# ' + room[player.location.name].discription + ' #')
+    print('\n' + ('#' * (4 + len(player.location.name))))
+
+def prompt():
+    print("\n" + "=========================")
+    print("What would you like to do?")
+    action = input("> ")
+    acceptable_actions = ['move', 'go','travel', 'walk', 'quit', 'examine', 'interact', 'inspect', 'look']
+    while action.lower() not in acceptable_actions:
+        print("Unknown action, try again. \n")
+        action = input("> ")
+    if action.lower() == 'quit':
+        sys.exit()
+    elif action.lower() in ['move', 'go','travel', 'walk']:
+        player_move(action.lower())
+    elif action.lower() in ['examine', 'interact', 'inspect', 'look']:
+        player_examine()
+
+def player_move(myAction):
+    ask = "Where would you like to move to? \n"
+    dest = input(ask)
+    if dest in ['up', 'north']:
+        destination = room[player.location.name].n_to
+        movement_handler(destination)
+    elif dest in ['left', 'west']:
+        destination = room[player.location.name].w_to
+        movement_handler(destination)
+    elif dest in ['right', 'east']:
+        destination = room[player.location.name].e_to
+        movement_handler(destination)
+    elif dest in ['down', 'south']:
+        destination = room[player.location.name].s_to
+        movement_handler(destination)
+
+def movement_handler(destination):
+    if destination == 'wall':
+        print('you have walked into a wall!!!')
+    else:
+        print("\n" + "You have moved to the" + destination.name + ".")
+        player.location = destination
+        print_location()
+
+def player_examine():
+    if len(room[player.location.name].item) is 0:
+        print("you have already exhuasted this room")
+    else:
+        for i in room[player.location].item:
+            print("You inspect the room and find " + i + "Would you like to take it?")
+            
+
+
+            
+
 
 # Make a new player object that is currently in the 'outside' room.
-play_name = input("Enter character name: ")
-player = Player(play_name, room['outside'])
+player = Player(room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
@@ -102,5 +152,65 @@ player = Player(play_name, room['outside'])
 #
 # If the user enters "q", quit the game.
 
+### Game Functionality ###
 def start_game():
-    return 
+    os.system('clear')
+
+    question1 = "Hello, what's your name? \n"
+    for character in question1:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    player_name = input("> ")
+    player.name = player_name
+    
+    question2 = "What role do you want to play? \n"
+    question2added = "You can play as a warrior, priest, or mage \n"
+    for character in question2:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    for character in question2added:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.01)
+    player_job = input("> ")
+    valid_jobs = ['warrior', 'mage', 'priest']
+    if player_job.lower() in valid_jobs:
+        player.job = player_job
+        print("\n you are now a " + player_job + "!")
+    while player_job not in valid_jobs:
+        player_job = input("> ")
+        if player_job.lower() in valid_jobs:
+            player.job = player_job
+            print("\n you are now a " + player_job + "!")
+
+    if player.job is 'warrior':
+        player.hp = 120
+        player.mp = 20
+    elif player.job is 'mage':
+        player.hp = 40
+        player.mp = 120
+    elif player.job is 'priest':
+        player.hp = 60
+        player.mp = 60
+    
+    ### intro ###
+
+    question3 = "Welcome " + player_name + " the" + player_job + "\n"
+    for character in question3:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+    os.system('clear')
+    print('lets start now')
+    main_game_loop()
+    
+    
+
+def main_game_loop():
+    while player.game_over is False:
+        prompt()
+
+title_screen()
--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player:
-    def __init__(self, name, location):
-        self.name = name
+    def __init__(self, location):
+        self.name = ''
         self.location = location
+        self.game_over = False
     def __str__(self):
         output = f'{self.name} {self.location}'
 
